057075e56cbd8268df1245f8c37355197c7de90b
literaryhero <andrew.lasher@gmail.com>
2024-04-02 13:55:18 +0900

    Beginning Descent into Madness
    Added first part of incremental
    1) Added some dialog
    2) Added autofact clicker
    3) Added sn(AI)l
    4) Added controls

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var list = [
+let list = [
     "Welcome to Snail Facts!<br> Click here to see a fact!",
     "Snails are gastropod mollusks, which means they belong to the same class as slugs and clams.",
     "Snails can be found in diverse habitats worldwide, including forests, deserts, and oceans.",
@@ -60,10 +60,184 @@ var list = [
     "Snails have a built-in compass in their shells that allows them to navigate long distances.",
     "Snails are excellent climbers and can scale vertical surfaces like walls and trees effortlessly.",
 ];
+let factsDoc = document.getElementById("facts");
+let posiDoc = document.getElementById("positive");
+let negiDoc = document.getElementById("negative");
+const factHistory = [];
+let inProcess = false;
+let speed = 1000;
+let click = 1;
+let basis = 1;
+let yesses = 0;
+let noes = 0;
+let autoTimer;
+factsDoc.innerHTML = list[0];
 
-document.getElementById("facts").innerHTML = list[0];
 
 function clickaroo() {
-    var randomFact = Math.floor((Math.random() * 58) + 1);
-    document.getElementById("facts").innerHTML = list[randomFact];
+    if (!inProcess) {
+        if (click === 2 | click === 4 | click === 6) {
+            chatTime();
+        } else {
+            randomFact();
+        }
+    }
+}
+
+function randomFact() {
+    let randomFact = Math.floor((Math.random() * 58) + 1);
+    factsDoc.innerHTML = click + ")" + " " + list[randomFact];
+    factHistory.push(list[randomFact]);
+    click++;
+}
+
+function chatTime() {
+    inProcess = true;
+    if (click === 2) {
+        factsDoc.innerHTML = "So, you like Snail Facts, huh?";
+        posiDoc.style.visibility = "visible";
+        negiDoc.style.visibility = "visible";
+    } else if (click === 4) {
+        endTimer();
+        factsDoc.innerHTML = "Wow, you are devouring facts like a snail devours vegetables!";
+        posiDoc.innerHTML = "...";
+        posiDoc.style.visibility = "visible";
+    } else if (click === 6) {
+        endTimer();
+        factsDoc.innerHTML = "Holy Snail! You are a Snail Fact collector!";
+        posiDoc.innerHTML = "And..."
+        posiDoc.style.visibility = "visible";
+    }
+}
+
+function yesClick() {
+    if (yesses === 0) {
+    document.getElementById("negative").style.visibility = "hidden";
+    document.getElementById("positive").innerHTML = "Thanks!";
+    factsDoc.innerHTML = "I like Snail Facts, too! To help you, I will activate AUTOFACTS™ so you don't have to click so much."
+    yesses = 1;
+    } else if (yesses === 1) {
+        document.getElementById("positive").style.visibility = "hidden";
+        yesses = 2;
+        randomFact();
+        autoTimer = setInterval(clickaroo, speed);
+        inProcess = false;
+    } else if (yesses === 2) {
+        factsDoc.innerHTML = "Since you seem to love Snail Facts as much as me, I will double the AUTOFACTS™ speed for you.";
+        posiDoc.innerHTML = "Let's Go!";
+        yesses = 3;
+    } else if (yesses === 3) {
+        posiDoc.style.visibility = "hidden";
+        inProcess = false;
+        yesses = 4;
+        randomFact();
+        speed = 500;
+        autoTimer = setInterval(clickaroo, speed);
+    } else if (yesses === 4) {
+        factsDoc.innerHTML = "Well, this is actually a little embarrassing, but we are running out of facts. How would you like to beta test our next gen artificial intelligence, called sn(AI)l?";
+        posiDoc.innerHTML = "I...I'd be honored.";
+        yesses = 5;
+    } else if (yesses === 5) {
+        factsDoc.innerHTML = "Great, as part of the service, we will again double the speed of AUTOFACTS™, and add some controls";
+        posiDoc.innerHTML = "I'm Ready.";
+        yesses = 6;
+    } else if (yesses === 6) {
+        posiDoc.style.visibility = "hidden";
+        inProcess = false;
+        yesses = 7;
+        snail();
+        document.getElementById("controls").style.display = "flex";
+        speed = 250;
+        autoTimer = setInterval(snail, speed);
+    }
+}
+
+function noClick() {
+    if (noes === 0) {
+    factsDoc.innerHTML = "You must have misclicked. Please click Yes!"
+    document.getElementById("negative").style.visibility = "hidden";
+    noes = 1;
+    }
+}
+
+function snail() {
+    var nextText = factGen();
+    factsDoc.innerHTML = click + ")" + " " + nextText;
+    factHistory.push(nextText);
+    click++;
+}
+
+function factGen() {
+    var verbs, nouns, modifier, adjectives, adverbs, preposition;
+    nouns = ["All snail varieties", "Snails", "Most snails", "Many types of snails", "All snail species", "Some snail species", "Many snails", "Some larger snails", "Only the smallest snails", "Two snail species"];
+    modifier = [" can", " will", " sometimes", " must", " have been known to", " are believed to", " on occasion", ", it has been said,", " have records that they", ""];
+    verbs = [" jump", " climb", " eat", " swim", " survive", " cross", " fly", " communicate", " raise the dead", " speak telepathically"];
+    adjectives = [" beautiful", " obscene", "", " lovely", " dumb", " rough", " soft", " hot", " vibrating", " slimy"];
+    adverbs = ["", " elegantly", " precisely", " quickly", " sadly", " humbly", " proudly", " shockingly", " calmly", " passionately"];
+    preposition = [" down", " into", " up", " on", " upon", " below", " above", " through", " across", " towards"];
+    object = [" deserts", " trees", " decaying organisms", " cakes", " ocean depths", " cobalt mines", " nether portals", " damp caves", " crevasses", " food sources"];
+
+    var rand1 = Math.floor(Math.random() * 10);
+    var rand2 = Math.floor(Math.random() * 10);
+    var rand3 = Math.floor(Math.random() * 10);
+    var rand4 = Math.floor(Math.random() * 10);
+    var rand5 = Math.floor(Math.random() * 10);
+    var rand6 = Math.floor(Math.random() * 10);
+    var rand7 = Math.floor(Math.random() * 10);
+    
+    var content = nouns[rand1] + modifier[rand2] + adverbs[rand3] + verbs[rand4] + preposition[rand5] + adjectives[rand6] + object[rand7] + ".";
+    
+    return(content);
+}
+
+function firstPage() {
+    endTimer();
+    basis = 1;
+    factsDoc.innerHTML = basis + ") " + factHistory[0];
+}
+
+function rewind() {
+    if (autoTimer != null) {
+        basis = click - 1;
+    }
+    endTimer();
+    if (basis > 1) {
+        basis--; } else {
+        basis = factHistory.length;
+    }
+    factsDoc.innerHTML = basis + ") " + factHistory[basis-1];
+}
+
+function play() {
+    endTimer();
+    click = factHistory.length + 1;
+    autoTimer = setInterval(snail, speed);
+}
+
+function pause() {
+    basis = click - 1;
+    endTimer();
+}
+
+function foward() {
+    if (autoTimer != null) {
+        basis = click - 1;
+    }
+    endTimer();
+    if (basis < (factHistory.length)) {
+        basis++; } else {
+        basis = 1;
+    }
+    factsDoc.innerHTML = basis + ") " + factHistory[basis-1];
+}
+
+function lastPage() {
+    endTimer();
+    basis = factHistory.length;
+    factsDoc.innerHTML = (click - 1) + ") " + factHistory[click-2];
+}
+
+function endTimer() {
+    clearInterval(autoTimer);
+    autoTimer = null;
 }
